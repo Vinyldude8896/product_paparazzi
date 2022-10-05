733dd350daa2b4a39e88c8719696cab6715fa96c
Kevin Reid <kevinnivek@me.com>
2022-10-05 00:21:41 -0400

    added login and signup auth

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME_BASIC } from '../utils/queries';
 
 const Home = () => {
+  // const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
+ // will import out photos here 
+  // const thoughts = data?.thoughts || [];
+
+  const loggedIn = Auth.loggedIn();
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            {/* <ThoughtForm /> */}
+            <h1>Let's upload some photos!</h1>
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {/* {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )} */}
+
+        </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+          </div>
+        ) : null}
       </div>
     </main>
   );
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,32 @@
-const { AuthenticationError } = require("apollo-server-express");
-const { User, Photo, Category, Order } = require("../models");
-const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // User: async (parent, args) => {
-    //     const usersData = await User.find({})
-    //     .select('-__v -password')
-    //     .populate('photos');
-
-    //     return usersData;
-    // },
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
-          .select("-__v -password")
-          .populate("thoughts")
-          .populate("friends");
+          .select('-__v -password')
+
 
         return userData;
       }
 
-      throw new AuthenticationError("Not logged in");
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+
     },
   },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -31,38 +34,36 @@ const resolvers = {
 
       return { token, user };
     },
-    addPhoto: async (parent, args, context) => {
-        if (context.user) {
-          const photo = await Photo.create({ ...args, username: context.user.username });
-      
-          await User.findByIdAndUpdate(
-            { _id: context.user._id },
-            { $push: { photos: photo._id } },
-            { new: true }
-          );
-      
-          return photo;
-        }
-      
-        throw new AuthenticationError('You need to be logged in!');
-      },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError('Incorrect credentials');
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError('Incorrect credentials');
       }
 
       const token = signToken(user);
       return { token, user };
     },
-  },
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  }
 };
 
 module.exports = resolvers;
