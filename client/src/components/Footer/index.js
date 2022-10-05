8f41cead829b044eef0aecf7f0d2be31ae65620b
Michelle <michelle.douma@gmail.com>
2022-10-05 19:08:10 -0400

    started css

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">
-        &copy;{new Date().getFullYear()} Created by Alexandre Savov | Fumi Tsukamoto | Michelle Douma | Kevin Reid
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="w-100 mt-auto bg-secondary p-4">
+			<div id="style.footer" className="container">
+				&copy;{new Date().getFullYear()} Created by Alexandre Savov | Fumi
+				Tsukamoto | Michelle Douma | Kevin Reid
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
