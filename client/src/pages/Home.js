cd638aeabbe7380749905d5c1a1b323aa2d3258f
Michelle <michelle.douma@gmail.com>
2022-10-09 13:42:30 -0400

    fixed double pic

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -71,53 +71,39 @@ const Home = () => {
 							alt="shopping cart in aisle"
 						/>
 
-						<main>
-							<div className="col-12 ">
-								{!loggedIn && (
-									<div>
-										<img
-											className=" myBackgroundImage"
-											src={BackgroundImage}
-											alt="shopping cart in aisle"
-										/>
-									</div>
-								)}
-
-								{loggedIn && (
-									<section className="m-10">
-										<div className="sm:text-center lg:text-left pb-3 z-10">
-											<h1 className="text-center text-4xl font-bold">
-												<span className="card-title text-dark">
-													Current Candids{" "}
-												</span>
-											</h1>
-										</div>
-										{CandidUploads.map((x) => {
-											return (
-												<PictureCard
-													key={x.key}
-													image={x.image}
-													productName={x.productName}
-													retailer={x.retailer}
-													dateUploaded={x.dateUploaded}
-												></PictureCard>
-											);
-										})}
-									</section>
-								)}
-								<div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
-									{!Auth.loggedIn() ? (
-										<>
-											<div class="bg-text">
-												<h2>Help Your Favourite Brand and Be Rewarded</h2>
-											</div>
-										</>
-									) : (
-										<></>
-									)}
+						{loggedIn && (
+							<section className="m-10">
+								<div className="sm:text-center lg:text-left pb-3 z-10">
+									<h1 className="text-center text-4xl font-bold">
+										<span className="card-title text-dark">
+											Current Candids{" "}
+										</span>
+									</h1>
 								</div>
-							</div>
-						</main>
+								{CandidUploads.map((x) => {
+									return (
+										<PictureCard
+											key={x.key}
+											image={x.image}
+											productName={x.productName}
+											retailer={x.retailer}
+											dateUploaded={x.dateUploaded}
+										></PictureCard>
+									);
+								})}
+							</section>
+						)}
+						<div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
+							{!Auth.loggedIn() ? (
+								<>
+									<div class="bg-text">
+										<h2>Help Your Favourite Brand and Be Rewarded</h2>
+									</div>
+								</>
+							) : (
+								<></>
+							)}
+						</div>
 					</div>
 				)}
 			</div>
