459d1ed12c318e2655ddeb054650f15dbb996b9c
Kevin Reid <kevinnivek@me.com>
2022-10-11 11:31:21 -0400

    added currentcandids component with wuery call

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,62 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Auth from "../utils/auth";
-import { useQuery } from "@apollo/client";
-import { QUERY_ME_BASIC } from "../utils/queries";
-import { PictureCard } from "../components/PictureCard";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import { QUERY_ME_BASIC , QUERY_CANDIDS} from "../utils/queries";
+import CandidList from "../components/CandidList";
 import BackgroundImage from "../images/shopcartaisle.jpeg";
-
-import Kombucha1 from "../images/Kombucha1.png";
-import Kombucha2 from "../images/Kombucha2.png";
-import Kombucha3 from "../images/Kombucha3.png";
-import Kombucha4 from "../images/Kombucha4.png";
-import Kombucha5 from "../images/Kombucha5.png";
-import Kombucha6 from "../images/Kombucha6.png";
-
-const CandidUploads = [
-  {
-    image: Kombucha1,
-    productName: "Kombucha1",
-    retailer: "Loblaws",
-    dateUploaded: new Date().getDay(),
-  },
-  {
-    image: Kombucha2,
-    productName: "Kombucha2",
-    retailer: "Metro",
-    dateUploaded: new Date().getMonth(),
-  },
-  {
-    image: Kombucha3,
-    productName: "Kombucha3",
-    retailer: "Sobeys",
-    dateUploaded: new Date().getMonth(),
-  },
-  {
-    image: Kombucha4,
-    productName: "Kombucha4",
-    retailer: "Fortinos",
-    dateUploaded: new Date().getMonth(),
-  },
-  {
-    image: Kombucha5,
-    productName: "Kombucha5",
-    retailer: "Fortinos",
-    dateUploaded: new Date().getMonth(),
-  },
-  {
-    image: Kombucha6,
-    productName: "Kombucha6",
-    retailer: "Fortinos",
-    dateUploaded: new Date().getMonth(),
-  },
-];
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
-  // const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { loading, data } = useQuery(QUERY_CANDIDS);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const locationKey = useLocation().key;
+  const user = Auth.getProfile().data;
 
   // will import out photos here
-  // const thoughts = data?.thoughts || [];
+const [getCandids, {loading: loadingCandids, data: candidsData, error}] = useLazyQuery(QUERY_CANDIDS, {
+  variables: { username: user.username}
+})
+
+useEffect(() => {
+  getCandids();
+}, [locationKey. getCandids]);
+
+if (loadingCandids || !candidsData) {
+  return <div>Loading....</div>
+}
+
+if (error) {
+  return <div>Error occured</div>
+}
 
   const loggedIn = Auth.loggedIn();
 
@@ -70,18 +41,13 @@ const Home = () => {
         )}
 
         {loggedIn && (
-          <section className="m-10">
-            <div className="sm:text-center lg:text-left pb-3 z-10">
-              <h1 className="text-center text-4xl font-bold">
-                <span className="card-title text-dark">Current Candids </span>
-              </h1>
-            </div>
-            {CandidUploads.map(x => {
-              return (
-                <PictureCard key={x.key} image={x.image} productName={x.productName} retailer={x.retailer} dateUploaded={x.dateUploaded}></PictureCard>
-              )
-            })}
-          </section>
+
+          <div className ="col-12 mb-3">
+          <CandidList
+          candids = {candidsData.candids}
+          title = "Current Candids"
+          />
+          </div>
         )}
         <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {!Auth.loggedIn() ? (
