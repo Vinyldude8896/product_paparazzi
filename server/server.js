3dca28d99888747b9efa3f545b60745e4bf295e3
Kevin Reid <kevinnivek@me.com>
2022-10-12 21:05:14 -0400

    added new build path

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use('/candid-photos', express.static(path.resolve(__dirname, './Photos')));
 app.get('/',(req, res) => {
-	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 }) 
 
 // Serve up static assets
